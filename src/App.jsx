c8979e241c7ae831f5d954ccd847ae02c31ef99a
Creative Mindset <tobiawotoye03@gmail.com>
2025-04-10 23:05:29 +0100

    added a gradient background to the hero page

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import Hero from "./components/Hero";
 const App = () => {
   return (
     <main className="relative min-h-screen overflow-x-hidden">
-      <NavBar />
-      <Hero />
+      <div className="absolute -top-20 -left-20 h-[500px] w-[500px] bg-gradient-to-tl from-blue-300/20 via-amber-500/20 to-pink-300/20 rounded-full blur-2xl -z-10"></div>
+      <div className="overflow-hidden">
+        <NavBar />
+        <Hero />
+      </div>
     </main>
   );
 };
